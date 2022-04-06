8fe9209c41f35404df91fdaff57933789bd1dee9
MarcelloAragoni <32502447+MarcelloAragoni@users.noreply.github.com>
2022-04-06 13:38:23 -0300

    Created card component and style (#2)
    **Trello:** https://trello.com/c/Ja6jTwGG/3-simple-react-component
    
    ## Description
    Created page Home, created the pastes, and the button component.
    
    ## Screenshot
    ![image](https://user-images.githubusercontent.com/32502447/162008075-c08f13b1-969c-443b-aac0-fea5d7af4915.png)
    ![image](https://user-images.githubusercontent.com/32502447/162008167-980290fa-f43a-494c-b97d-3a1d2c2fab37.png)

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,5 +1,17 @@
 import "./Button.scss";
+import cn from "classnames";
 
-export default function Button({text, type = "primary"}) {
-  return <button className={`button button--${type}`}>{text}</button>;
+export default function Button({ text, color }) {
+  const buttonColor = cn(
+    "button",
+    { "button--orange": color === "orange" },
+    { "button--green": color === "green" },
+    { "button--blue": color === "blue" }
+  );
+
+  return (
+    <button type="button" className={buttonColor}>
+      {text}
+    </button>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,26 @@
-import Button from "../components/button/Button";
+import Card from "../components/Card/Card";
 
 export default function Home() {
   return (
     <>
-      <h1>SEDANS</h1>
-      <p>
-        Choose a sedan for its affodability and excellent fuel economy. Ideal
-        for cruising in the city or your next road trip.
-      </p>
-      <Button text="Learn More" />
+      <Card
+        imageType="sedan"
+        title="SEDANS"
+        text="Choose a sedan for its affodability and excellent fuel economy. Ideal for cruising in the city or your next road trip."
+        buttonColor="orange"
+      />
+      <Card
+        imageType="suvs"
+        title="SUVS"
+        text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+        buttonColor="green"
+      />
+      <Card
+        imageType="luxury"
+        title="LUXURY"
+        text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style"
+        buttonColor="blue"
+      />
     </>
   );
 }
