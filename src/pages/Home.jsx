18f1798763e22cee6633d94054fff50b5b744b27
Marcello <marcello_aragoni@yahoo.com.br>
2022-04-06 15:59:05 -0300

    Cards Panel and home style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,11 @@
-import Card from "../components/Card/Card";
+import Cards from "../components/Cards/Cards";
+
+import "./Home.scss";
 
 export default function Home() {
   return (
-    <>
-      <Card
-        imageType="sedan"
-        title="SEDANS"
-        text="Choose a sedan for its affodability and excellent fuel economy. Ideal for cruising in the city or your next road trip."
-        buttonColor="orange"
-      />
-      <Card
-        imageType="suvs"
-        title="SUVS"
-        text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
-        buttonColor="green"
-      />
-      <Card
-        imageType="luxury"
-        title="LUXURY"
-        text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style"
-        buttonColor="blue"
-      />
-    </>
+    <div className="home">
+      <Cards />
+    </div>
   );
 }
