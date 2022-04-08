3f1901f578b607c92a83ce447dc1dc30d24dfc55
Marcello <marcello_aragoni@yahoo.com.br>
2022-04-08 20:33:39 -0300

    implementing fetch

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,21 +1,31 @@
+import { useEffect, useState } from "react";
 import Card from "../Card/Card";
-import automobile from "../../data/data.json";
 
 import "./Cards.scss";
 
 export default function Cards() {
+  const [automobiles, setAutomobiles] = useState([]);
+
+  useEffect(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAutomobiles(data.automobiles);
+      });
+  }, []);
+
   return (
     <div className="cards__panel">
-      {automobile.map((props) => {
-        return (
-          <Card
-            imageType={props.type}
-            title={props.title}
-            text={props.text}
-            buttonColor={props.buttonColor}
-          />
-        );
-      })}
+      {automobiles.map((automobile) => (
+        <Card
+          key={automobile.id}
+          imageType={automobile.type}
+          title={automobile.title}
+          text={automobile.text}
+          buttonColor={automobile.buttonColor}
+        />
+      ))}
     </div>
   );
 }
