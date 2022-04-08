a2b3471df7a64f20c9d40bdd5bf0e4fa066456db
Marcello <marcello_aragoni@yahoo.com.br>
2022-04-08 12:45:24 -0300

    json props

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,28 +1,21 @@
 import Card from "../Card/Card";
+import automobile from "../../data/data.json";
 
 import "./Cards.scss";
 
 export default function Cards() {
   return (
     <div className="cards__panel">
-      <Card
-        imageType="sedan"
-        title="SEDANS"
-        text="Choose a sedan for its affodability and excellent fuel economy. Ideal for cruising in the city or your next road trip."
-        buttonColor="orange"
-      />
-      <Card
-        imageType="suvs"
-        title="SUVS"
-        text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
-        buttonColor="green"
-      />
-      <Card
-        imageType="luxury"
-        title="LUXURY"
-        text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style"
-        buttonColor="blue"
-      />
+      {automobile.map((props) => {
+        return (
+          <Card
+            imageType={props.type}
+            title={props.title}
+            text={props.text}
+            buttonColor={props.buttonColor}
+          />
+        );
+      })}
     </div>
   );
 }
