eaa8db18911ed11e8874615fb392fba11c986cef
MarcelloAragoni <32502447+MarcelloAragoni@users.noreply.github.com>
2022-04-12 15:35:40 -0300

    json props (#4)
    **Trello:** https://trello.com/c/Ja6jTwGG/3-simple-react-component
    
    ## Description
    add json file and implemented with map
    
    ## Screenshot
    ![image](https://user-images.githubusercontent.com/32502447/162977362-617454d8-3504-4f13-b5b5-5337cccc6a5b.png)

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,28 +1,30 @@
+import { useEffect, useState } from "react";
 import Card from "../Card/Card";
 
 import "./Cards.scss";
 
 export default function Cards() {
+  const [automobiles, setAutomobiles] = useState([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setAutomobiles(data.automobiles);
+      });
+  }, []);
+
   return (
     <div className="cards__panel">
-      <Card
-        imageType="sedan"
-        title="SEDANS"
-        text="Choose a sedan for its affodability and excellent fuel economy. Ideal for cruising in the city or your next road trip."
-        buttonColor="orange"
-      />
-      <Card
-        imageType="suvs"
-        title="SUVS"
-        text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
-        buttonColor="green"
-      />
-      <Card
-        imageType="luxury"
-        title="LUXURY"
-        text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style"
-        buttonColor="blue"
-      />
+      {automobiles.map((automobile) => (
+        <Card
+          key={automobile.id}
+          imageType={automobile.type}
+          title={automobile.title}
+          text={automobile.text}
+          buttonColor={automobile.buttonColor}
+        />
+      ))}
     </div>
   );
 }
